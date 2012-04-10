4c954ae93c84a54dfc1dcc9a733e5fa37afc6de9
Varun Murthy <varun.murthy@gmail.com>
2012-04-10 00:31:44 -0400

    Finished Challenge 3

--- Challenge3.py
+++ Challenge3.py
@@ -1,1 +1,12 @@
+import re
 f = open('./Challenge3.txt','r')
+list = []
+
+for x in  f:
+    m = (re.search('[^A-Z][A-Z][A-Z][A-Z][a-z][A-Z][A-Z][A-Z][^A-Z]',x))
+    if m:
+        list.append((m.group(0)[4]))
+
+print ''.join(list)
+
+     
